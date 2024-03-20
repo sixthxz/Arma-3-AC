edbb15bdd888c3d07c5e925dcda88edb652bf2db
sixthxz <145708241+sixthxz@users.noreply.github.com>
2024-03-20 14:44:19 -0600

    Code restructuring for modularity and usability enhancements.
    -The original code structure was reorganized to enhance modularity and maintainability. Functions were introduced to encapsulate specific tasks, such as map initialization, adding markers, calculating distances, and updating popups. This modular approach improves code readability and makes it easier to understand and manage different parts of the application.
    - Tooltips were added to the buttons in the user interface to provide descriptive hints about their functionality when users hover over them. This improves the usability of the application by providing users with clear guidance on the actions they can perform.
    
    - More structured, modular, and user-friendly form, enhancing its maintainability and usability.

--- js/map-engine.js
+++ js/map-engine.js
@@ -1,75 +1,193 @@
-/*
-Map and interactions
-*/
-
 $(function () {
     var mapInfos = Arma3Map.Maps.altis;
-
-    // Create map control
-    var map = L.map("map", {
-        minZoom: mapInfos.minZoom,
-        maxZoom: 15,
-        crs: mapInfos.CRS,
-    });
-
-    // Define tile layer
-    L.tileLayer.fallback(
-        "https://jetelain.github.io/Arma3Map" + mapInfos.tilePattern,
-        {
-            attribution: mapInfos.attribution,
-            tileSize: mapInfos.tileSize,
-        }
-    ).addTo(map);
-
-    // Center map, and sets default zoom
-    map.setView(mapInfos.center, mapInfos.defaultZoom);
-
-    // Add marker cluster group
+    var map = initializeMap(mapInfos);
     var markers = L.markerClusterGroup();
     map.addLayer(markers);
-
     var startPoint = null;
-
-    // Add grid sliders
-    L.latlngGraticule().addTo(map);
-
-    // Add a scale control
-    L.control.scale({ maxWidth: 200, imperial: false }).addTo(map);
-
-    // Add mouse grid position
     var gridMousePositionControl = L.control.gridMousePosition().addTo(map);
+    var polyline = null; // Definir la variable polyline aquí
 
-   map.on('contextmenu', function(e) {
-    if (startPoint === null) {
-        startPoint = e.latlng;
-        // Battery Marker
-        var marker = L.marker(startPoint, {icon: customIcon2}).addTo(map);
-        // Battery Marker Popup info 
-        marker.bindPopup(`
+var markers = L.markerClusterGroup({
+    maxClusterRadius: 1 // Ajusta el radio máximo de agrupación en píxeles
+});
+    map.on('contextmenu', function(e) {
+        if (startPoint === null) {
+            startPoint = e.latlng;
+            addStartMarker(startPoint, gridMousePositionControl);
+        } else {
+            var endPoint = e.latlng;
+            addMarkerAndDistance(startPoint, endPoint, markers, gridMousePositionControl, map);
+            addPolyline(startPoint, endPoint, map);
+        }
+    });
+
+    function initializeMap(mapInfos) {
+        var map = L.map("map", {
+            minZoom: mapInfos.minZoom,
+            maxZoom: 15,
+            crs: mapInfos.CRS,
+        });
+
+        L.tileLayer.fallback(
+            "https://jetelain.github.io/Arma3Map" + mapInfos.tilePattern,
+            {
+                attribution: mapInfos.attribution,
+                tileSize: mapInfos.tileSize,
+            }
+        ).addTo(map);
+
+        map.setView(mapInfos.center, mapInfos.defaultZoom);
+
+        L.latlngGraticule().addTo(map);
+        L.control.scale({ maxWidth: 200, imperial: false }).addTo(map);
+
+        return map;
+    }
+
+    function addStartMarker(startPoint, gridMousePositionControl) {
+        var marker = L.marker(startPoint, { icon: customIcon2 }).addTo(map);
+        marker.bindPopup(generatePopupContent(gridMousePositionControl)).openPopup();
+    }
+
+    function addMarkerAndDistance(startPoint, endPoint, markers, gridMousePositionControl, map) {
+        var distance = calculateDistance(endPoint.lat, endPoint.lng, startPoint.lat, startPoint.lng);
+        var marker = L.marker(endPoint, { icon: customIcon, draggable: true }).addTo(map);
+        marker.bindPopup(generatePopupContent(gridMousePositionControl, distance)).openPopup().addTo(markers);
+        marker.on('dragend', function(event){
+            updatePopupContent(marker, startPoint, gridMousePositionControl);
+            if (polyline) {
+                map.removeLayer(polyline); // Eliminar la polyline existente si está definida
+            }
+            var newEndPoint = event.target.getLatLng();
+            addPolyline(startPoint, newEndPoint, map); // Agregar la nueva polyline desde startPoint hasta newEndPoint
+        });
+    }
+
+    function addPolyline(startPoint, endPoint, map) {
+        polyline = L.polyline([startPoint, endPoint], { color: 'red' }).addTo(map); // Asignar la nueva polyline a la variable polyline
+    }
+
+       function generatePopupContent(gridMousePositionControl, distance) {
+        var content = `
             <table>
-                <tr><th colspan="2" style="text-align: justify;">Coordinates</th></tr>
+                <tr><th colspan="2" style="text-align: center;">Coordinates</th></tr>
                 <tr><td>X</td><td>Y</td></tr>
                 <tr><td>${gridMousePositionControl._container.innerText.split(' - ')[0]}</td><td>${gridMousePositionControl._container.innerText.split(' - ')[1]}</td></tr>
             </table>
-        `).openPopup();
-    } else {
-        var endPoint = e.latlng;
-        var distance = ((calculateDistance(endPoint.lat, endPoint.lng, startPoint.lat, startPoint.lng) / 10)).toFixed(1);
-        // Create marker and distance line
-        L.marker(endPoint, {icon: customIcon})
-            .bindPopup(`
-                <table>
-                    <tr><th colspan="2" style="text-align: center;">Coordinates</th></tr>
-                    <tr><td>X</td><td>Y</td></tr>
-                    <tr><td>${gridMousePositionControl._container.innerText.split(' - ')[0]}</td><td>${gridMousePositionControl._container.innerText.split(' - ')[1]}</td></tr>
-                    <tr><th colspan="2" style="text-align: center;">Distance</th></tr>
-                    <tr><td colspan="2" style="text-align: center;">${distance} m</td></tr>
-                </table>
-            `).openPopup()
-            .addTo(markers);
-        L.polyline([startPoint, endPoint], { color: 'red' }).addTo(map);
-    
+        `;
+        if (distance !== undefined) {
+            content += `
+                <tr><th colspan="2" style="text-align: center;">Distance</th></tr>
+                <tr><td colspan="2" style="text-align: center;">${(distance / 10).toFixed(1)} m</td></tr>
+            `;
+        }
+        return content;
     }
-});
+
+    function updatePopupContent(marker, startPoint, gridMousePositionControl) {
+        var newLatLng = marker.getLatLng();
+        var distance = calculateDistance(newLatLng.lat, newLatLng.lng, startPoint.lat, startPoint.lng);
+        var popupContent = generatePopupContent(gridMousePositionControl, distance);
+        marker.setPopupContent(popupContent);
+    }
+
+function updatePopupContentFromInput(marker, x, y) {
+    console.log("Valor de x:", x);
+    console.log("Valor de y:", y);
+    // Verificar si x e y son números antes de llamar a toFixed()
+    if (typeof x === 'number' && typeof y === 'number') {
+        // Convertir los números a enteros y luego a cadenas para eliminar los ceros adicionales
+        var xString = x.toFixed(0);
+        var yString = y.toFixed(0);
+        
+        // Eliminar el último carácter (que es el cero extra) de las coordenadas
+        xString = xString.slice(0, -1);
+        yString = yString.slice(0, -1);
+        
+        // Crear el contenido del popup con las coordenadas redondeadas a enteros y sin el cero extra
+        var content = `
+            <table>
+                <tr><th colspan="2" style="text-align: center;">Coordinates</th></tr>
+                <tr><td>X</td><td>Y</td></tr>
+                <tr><td>${xString}</td><td>${yString}</td></tr>
+            </table>
+        `;
+        marker.bindPopup(content).openPopup(); // Asociar el contenido del popup al marcador y abrirlo
+    } else {
+        console.log("Coordenadas inválidas.");
+    }
+}
+
+
+
+
+
+
+
+function addMarker1() {
+    // Elimina cualquier marcador inicial existente
+    if (startPoint !== null) {
+        map.removeLayer(startPoint);
+        startPoint = null;
+    }
+    
+    // Agrega un controlador de eventos para el clic en el mapa
+    map.once('click', function(e) {
+        startPoint = e.latlng;
+        addStartMarker(startPoint, gridMousePositionControl);
+    });
+}
+
+function addMarker2() {
+
+     // Elimina cualquier marcador inicial existente
+    if (startPoint !== null) {
+        map.removeLayer(startPoint);
+        startPoint = null;
+    }
+
+    var x = prompt("Ingrese la coordenada X (longitud):");
+    var y = prompt("Ingrese la coordenada Y (latitud):");
+
+    // Agregar un cero extra a las coordenadas si es necesario
+    x = addExtraZero(x);
+    y = addExtraZero(y);
+    
+    // Verificar si las coordenadas son válidas (números enteros)
+    if (!isNaN(parseFloat(x)) && !isNaN(parseFloat(y))) {
+        startPoint = L.latLng(parseFloat(y), parseFloat(x));
+        marker = L.marker(startPoint, { icon: customIcon2 }).addTo(map);
+        updatePopupContentFromInput(marker, parseFloat(x), parseFloat(y)); // Actualizar el contenido del popup
+    } else {
+        console.log("Coordenadas inválidas.");
+    }
+}
+
+
+
+function addExtraZero (coord) {
+    // Convertir la coordenada a un formato adecuado
+    if (coord.length <= 4) { // Si la longitud es menor o igual a 4, asumir que es un valor de cuatro dígitos
+        return coord + "0"; // Agregar un cero extra
+    }
+    return coord; // Devolver la coordenada sin cambios
+}
+
+
+
+
+
+
+
+
+new L.cascadeButtons([
+    {icon: 'fa fa-bars', title:'Menu', items:[
+        {icon: 'fa fa-map-pin', title: 'New Initial Marker (Left Click)', command: () =>{addMarker1()}},
+        {icon: 'fa fa-crosshairs', title: 'Input Coordinates', command: () =>{addMarker2()}},
+        {icon: 'fa fa-globe', title: 'Grid (Working on it)', command: () =>{console.log('button3')}},
+    ]}
+], {position:'topleft', direction:'horizontal'}).addTo(map);
+
+
 
 });
